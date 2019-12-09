2d32efe66e795d66abe32512e55b9a491179fe8c
LTY <1395785129@qq.com>
2019-12-09 10:28:34 +0800

    第8次提交，修改POM文件

---
+++ src/main/java/com/scs/web/blog/domain/dto/SimpleTopic.java
@@ -0,0 +1,16 @@
+package com.scs.web.blog.domain.dto;
+
+import lombok.Data;
+/**
+ * @author liu tianyuan
+ * @ClassName
+ * @Description
+ * @Date 2019/12/9
+ * @Version 1.0
+ **/
+@Data
+public class SimpleTopic {
+    private Long id;
+    private String topicName;
+    private String logo;
+}
